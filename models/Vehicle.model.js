8b9f6aa441375545929c4de472d44003b92653b5
ProgrammingDj <djamelpettes@gmail.com>
2023-06-29 10:41:59 +0200

    Vehicle model done

---
+++ models/Vehicle.model.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const vehicleSchema = new Schema(
+  {
+    vehicle: {
+        type: String,
+        required: [true, "Vehicle is required."],
+      },
+    vehicleImage: {
+      type: Image,
+    },
+    probationaryDriversLicense: {
+            type: String,
+            enum: ["Yes", "No",],
+          },
+    carsharing: {
+            type: String,
+            enum: ["Yes", "No",],
+          },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Vehicle = model("Vehicle", vehicleSchema);
+
+module.exports = Vehicle;
