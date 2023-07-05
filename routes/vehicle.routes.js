d9708e16cde4021fbe9253a07d865e402e469dad
ProgrammingDj <djamelpettes@gmail.com>
2023-07-05 12:45:01 +0200

    update vehicle

--- routes/vehicle.routes.js
+++ routes/vehicle.routes.js
@@ -1,11 +1,23 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
-
+const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 const Vehicle = require("../models/Vehicle.model");
 const User = require("../models/User.model");
 
+router.get("/vehicle", isAuthenticated, (req, res, next) => {
+  Vehicle.find({owner: req.payload._id})
+  .then((userVehicles) => { res.json(userVehicles);
+  })
+  .catch((err) => {
+    res.status(500).json({
+      message: "error trying to get a vehicle",
+      error: err,
+  })
+})
+})
+
 //  POST /api/tasks  -  Creates a new vehicle
-router.post("/vehicles", (req, res, next) => {
+router.post("/vehicle", isAuthenticated,(req, res, next) => {
   const { vehicle, vehicleImage } = req.body;
 
   const newVehicleDetails = {
@@ -15,11 +27,6 @@ router.post("/vehicles", (req, res, next) => {
   };
 
   Vehicle.create(newVehicleDetails)
-    .then((vehicleFromDB) => {
-      return User.findByIdAndUpdate(userId, {
-        $push: { vehicle: vehicleFromDB._id },
-      });
-    })
     .then((response) => res.status(201).json(response))
     .catch((err) => {
       console.log("error creating a new vehicle", err);
@@ -29,5 +36,19 @@ router.post("/vehicles", (req, res, next) => {
       });
     });
 });
+
+router.put('/rides/vehicle', (req, res, next) => {
+  const { vehicleId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(vehicleId)) {
+      res.status(400).json({ message: 'Specified vehicle id is not valid' });
+      return;
+  }
+
+  const newVehicleDetails = {
+      vehicle: req.body.vehicle,
+      vehicleImage: req.body.vehicleImage,
+  }
+})
 
 module.exports = router;
