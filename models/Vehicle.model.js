cb76c1713577e1eee46d4907f578c69850a4adc8
Eirik Klebo <eiriktobias@gmail.com>
2023-07-06 15:34:15 +0200

    fixing image upload for deployd

--- models/Vehicle.model.js
+++ models/Vehicle.model.js
@@ -13,7 +13,7 @@ const vehicleSchema = new Schema(
     imageUrl: {
       type: String,
       default:
-        "https://www.mivodo.com/wp-content/uploads/2021/02/seat-alhambra-grosser-van-fuer-familien.jpg",
+        "https://t3.ftcdn.net/jpg/04/62/93/66/360_F_462936689_BpEEcxfgMuYPfTaIAOC1tCDurmsno7Sp.jpg",
     },
   },
   {
